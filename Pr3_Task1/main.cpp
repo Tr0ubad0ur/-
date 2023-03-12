4a4f2576f875f1117679343e497b420e30ed1073
Tr0ubad0ur <125650555+Tr0ubad0ur@users.noreply.github.com>
2023-03-12 23:08:25 +0300

    Update main.cpp

--- Pr3_Task1/main.cpp
+++ Pr3_Task1/main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include "eq2.h"
 
+using namespace std;
+
+int main()
+{
+
+	eq2 u1(2, -53, 12);
+	eq2 u2(-3, -35, 14);
+	eq2 u3;
+
+	u3 = u1 + u2;
+
+	cout << "Решение первого уравнения  : " << u1.find_X() << endl;
+	cout << "Решение второго уравнения  : " << u2.find_X() << endl;
+	cout << "Решение третьего уравнения : " << u3.find_X() << endl;
+
+	double ARG = 5;
+
+	cout << "Значение первого уравнения при  X = " << ARG << " = " << u1.find_Y(ARG) << endl;
+	cout << "Значение второго уравнения при  X = " << ARG << " = " << u2.find_Y(ARG) << endl;
+	cout << "Значение третьего уравнения при X = " << ARG << " = " << u3.find_Y(ARG) << endl;
+
+	return 0;
+}
