99620e079b4005c8bb25baa0fca34a4d67fed9bc
Tr0ubad0ur <125650555+Tr0ubad0ur@users.noreply.github.com>
2023-03-28 18:38:00 +0300

    Update Ocean.cpp

--- Practice6/Task3/Ocean.cpp
+++ Practice6/Task3/Ocean.cpp
@@ -1,1 +1,25 @@
+#include "Ocean.h"
 
+void Ocean::addSea()
+{
+	Sea* subarray = new Sea[seas_count + 1];
+	for (int i = 0; i < seas_count; i++)
+	{
+		subarray[i] = seas[i];
+	}
+	seas_count++;
+	seas = subarray;
+}
+
+void Ocean::setName(string N)
+{
+	name = N;
+}
+void Ocean::setDeep(float D)
+{
+	deep = D;
+}
+void Ocean::setSize(float S)
+{
+	size = S;
+}
