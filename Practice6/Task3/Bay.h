edb55adea9c6f53e1717a1871d4f1c7001f42ef2
Tr0ubad0ur <125650555+Tr0ubad0ur@users.noreply.github.com>
2023-03-28 18:35:09 +0300

    Update Bay.h

--- Practice6/Task3/Bay.h
+++ Practice6/Task3/Bay.h
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Bay
+{
+private:
+	string name;
+	float deep;
+	float size;
+
+	string NamesBay[12] = {
+	"Джинанский", "Ворстонский", "Новорассветный", "Байкальский", "Ломоносовский", "Тропический", "Трамиский", "Апрельский", "Безымянный", "Холодный", "Маленький", "Тормонский"
+	};
+
+public:
+	Bay()
+	{
+		name = NamesBay[rand() % 12];
+		deep = rand() % 6000 + 4000;
+		size = (rand() % 9500 + 2500) / 10.0;
+	}
+	Bay(string N, float D, float S)
+	{
+		name = N;
+		deep = D;
+		size = S;
+	}
+
+	void setName(string N);
+	void setDeep(float D);
+	void setSize(float S);
+
+};
 
