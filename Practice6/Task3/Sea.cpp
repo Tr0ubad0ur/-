cbb284cfdb48153e32af84570a3c780b2d981e3b
Tr0ubad0ur <125650555+Tr0ubad0ur@users.noreply.github.com>
2023-03-28 18:44:34 +0300

    Update Sea.cpp

--- Practice6/Task3/Sea.cpp
+++ Practice6/Task3/Sea.cpp
@@ -1,1 +1,25 @@
+#include "Sea.h"
 
+void Sea::addBay()
+	{
+	Bay* subarray = new Bay[Bays_count + 1];
+	for (int i = 0; i < Bays_count; i++)
+	{
+		subarray[i] = Bays[i];
+	}
+	Bays_count++;
+	Bays = subarray;
+	}
+
+void Sea::setName(string N)
+{
+	name = N;
+}
+void Sea::setDeep(float D)
+{
+	deep = D;
+}
+void Sea::setSize(float S)
+{
+	size = S;
+}
