6b892dba4184c287d9ba9669b34b28f82ab48fcd
Tr0ubad0ur <125650555+Tr0ubad0ur@users.noreply.github.com>
2023-03-28 18:25:19 +0300

    Update main.cpp

--- Practice6/Task2/main.cpp
+++ Practice6/Task2/main.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include "subcone.h"
 
+using namespace std;
+
+int main()
+{
+	
+	int size;
+	cout << "Введите кол-во усечённых конусов\n";
+	cin >> size;
+
+	subcone* C = new subcone[size];
+	
+	cout << endl;
+	cout << "Введите усечённый конус в формате {x y z R H subH}\n";
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Введите данные усчённого конуса номер " << i + 1 << ": ";
+		cin >> C[i];
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << endl;
+		cout << "Данные конуса номер " << i + 1 << ":\n";
+		cout << C[i] << endl;
+		cout << "S = " << C[i].area() << endl;
+		cout << "V = " << C[i].volume() << endl;
+	}
+
+	return 0;
+}
