fa2683722c554f8477be22b6db82d2d33ed6648c
Tr0ubad0ur <125650555+Tr0ubad0ur@users.noreply.github.com>
2023-03-28 18:49:08 +0300

    Update main.cpp

--- Practice6/Task3/main.cpp
+++ Practice6/Task3/main.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+#include <ctime>
 
+#include "Ocean.h"
+
+using namespace std;
+
+int main()
+{
+	
+	srand(time(NULL));
+
+	Ocean f1,f2,f3,f4;
+	f1.addSea();
+	f1.addSea();
+	f1.seas[0].addBay();
+	f1.seas[0].addBay();
+	f1.seas[1].addBay();
+	f1.seas[1].addBay();
+	f1.seas[1].addBay();
+	f3.addSea();
+
+	cout << f1 << endl;
+	cout << f2 << endl;
+	cout << f3 << endl;
+
+	f4.addSea();
+	f4.seas[0].addBay();
+	
+	cout << "\nВведите данные океана (Название, глубина, размер):";
+	cin >> f4;
+	cout << "Введите данные моря (Название, глубина, размер):";
+	cin >> f4.seas[0];
+	cout << "Введите данные залива (Название, глубина, размер):";
+	cin >> f4.seas[0].Bays[0];
+	cout << endl;
+	cout << f4;
+	cout << endl;
+
+	return 0;
+}
