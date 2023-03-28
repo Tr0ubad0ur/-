1ff7b7ab9e17fbd4894f2fa1362292670c4903eb
Tr0ubad0ur <125650555+Tr0ubad0ur@users.noreply.github.com>
2023-03-28 18:10:16 +0300

    Update cone.h

--- Practice6/Task1/cone.h
+++ Practice6/Task1/cone.h
@@ -1,50 +1,50 @@
-#include <cmath>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 class cone
 {
-private:
-	const double PI = 3.141592;
-	double x, y, z;
-	double R;
-	double H;
-public:
-	cone()
-	{
-		x = 0;
-		y = 0;
-		z = 0;
-		R = 0;
-		H = 0;
-	}
-	cone(double r, double h)
-	{
-		x = 0;
-		y = 0;
-		z = 0;
-		R = r;
-		H = h;
-	}
-	cone(double X, double Y, double Z, double r, double h)
-	{
-		x = X;
-		y = Y;
-		z = Z;
-		R = r;
-		H = h;
-	}
+	private:
+		const double PI = 3.141592;
+		double x, y, z;
+		double R;
+		double H;
+	public:
+		cone()
+		{
+			x = 0;
+			y = 0;
+			z = 0;
+			R = 0;
+			H = 0;
+		}
+		cone(double r, double h)
+		{
+			x = 0;
+			y = 0;
+			z = 0;
+			R = r;
+			H = h;
+		}
+		cone(double X, double Y, double Z, double r, double h)
+		{
+			x = X;
+			y = Y;
+			z = Z;
+			R = r;
+			H = h;
+		}
 
-	void setCoordinate(double X, double Y, double Z);
-	void setRadius(double r);
-	void setHeight(double h);
-	void getCoordinate(double& X, double& Y, double& Z);
-	double getRadius();
-	double getHeight();
+		void setCoord(double X, double Y, double Z);
+		void setRad(double r);
+		void setHeight(double h);
+		void getCoord(double& X, double& Y, double& Z);
+		double getRad();
+		double getHeight();
 
-	double area();
-	double volume();
+		double area();
+		double volume();
 
-	friend ostream& operator<<(ostream& stream, cone obj);
+		friend ostream& operator<<(ostream& stream, cone obj);
 };
