751b64c8d7cc68c13b9b810183ba14c3c45e3cea
Tr0ubad0ur <125650555+Tr0ubad0ur@users.noreply.github.com>
2023-03-28 18:22:00 +0300

    Update cone.cpp

--- Practice6/Task2/cone.cpp
+++ Practice6/Task2/cone.cpp
@@ -1,1 +1,39 @@
+#include "cone.h"
 
+void cone::setCoord(double X, double Y, double Z)
+{
+	x = X;
+	y = Y;
+	z = Z;
+}
+void cone::setRad(double r)
+{
+	R = r;
+}
+void cone::setHeight(double h)
+{
+	H = h;
+}
+void cone::getCoord(double& X, double& Y, double& Z)
+{
+	X = x;
+	Y = y;
+	Z = z;
+}
+double cone::getRad()
+{
+	return R;
+}
+double cone::getHeight()
+{
+	return H;
+}
+
+double cone::area()
+{
+	return PI * R * (R + sqrt(H * H + R * R));
+}
+double cone::volume()
+{
+	return PI * R * R * H / 3;
+}
